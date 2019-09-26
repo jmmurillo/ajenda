742559f9ac96b8c3d31b87b797d06ab6553e6750
murillo <jmurillo@rfranco.com>
2019-09-26 11:51:24 +0200

    Added ZonedDateTime return for Clock

--- src/main/java/org/murillo/ajenda/dto/Clock.java
+++ src/main/java/org/murillo/ajenda/dto/Clock.java
@@ -1,9 +1,22 @@
 package org.murillo.ajenda.dto;
 
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 public interface Clock {
 
-    default long nowEpochMs(){return System.currentTimeMillis();};
+    default long nowEpochMs() {
+        return System.currentTimeMillis();
+    }
 
-    default boolean shutdown(long gracePeriod){return true;}
+    default ZonedDateTime now() {
+        return ZonedDateTime.ofInstant(
+                Instant.ofEpochMilli(this.nowEpochMs()), ZoneOffset.UTC);
+    }
+
+    default boolean shutdown(long gracePeriod) {
+        return true;
+    }
 
 }
